b46f4f59077bb6c448b9085dabbded169b68a42c
ndibeifeanyi609-cmd <ndibeifeanyi609@gmail.com>
2026-07-23 21:41:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ChatTBM server is running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
