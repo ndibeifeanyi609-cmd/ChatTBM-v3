a27a891105dd8a2a225ad69dc6f32d58cc6cfaeb
ndibeifeanyi609-cmd <ndibeifeanyi609@gmail.com>
2026-08-02 15:05:12 +0100

    Update creatorRoutes.js

--- routes/creatorRoutes.js
+++ routes/creatorRoutes.js
@@ -1,1 +1,16 @@
+// =====================================
+// ChatTBM V6.8.3
+// Creator Routes
+// =====================================
 
+
+const express = require("express");
+
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
