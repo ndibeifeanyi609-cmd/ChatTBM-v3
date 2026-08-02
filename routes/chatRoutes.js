6a4420e438c607848cfbc00c535f22c775a77b55
ndibeifeanyi609-cmd <ndibeifeanyi609@gmail.com>
2026-08-02 15:04:49 +0100

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,1 +1,16 @@
+// =====================================
+// ChatTBM V6.8.3
+// Chat Routes
+// =====================================
 
+
+const express = require("express");
+
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
